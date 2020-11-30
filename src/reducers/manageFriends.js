1162d7e79642499f8f6f7fe65a4cfeab398e06b3
Justin_Webb <Webb.n.justin@gmail.com>
2020-11-30 13:27:37 -0600

    practice state change basics

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type){
+        case 'ADD_FRIEND':
+            return ( {...state, friends: [
+                ...state.friends, 
+                action.friend
+            ]})
+        case 'REMOVE_FRIEND':
+           const friendToRemove = state.friends.findIndex(friend => friend.id === action.id)
+            return({
+                // nonmutative
+                ...state, 
+                friends: [
+                    // takes all the friends before the one to remove 
+                    ...state.friends.slice(0, friendToRemove),
+                    // takes all the friends after the one to remove 
+                    ...state.friends.slice(friendToRemove + 1)
+                ]
+            })
+        default: 
+            return state;
+    }
 }
